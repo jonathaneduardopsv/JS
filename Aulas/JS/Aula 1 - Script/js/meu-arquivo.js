e49141c29ef1337ee040a12ef0e5926ca58dabdf
Jonathan Eduardo <106741029+jonathaneduardopsv@users.noreply.github.com>
2022-10-21 22:34:20 -0300

    js

---
+++ Aulas/JS/Aula 1 - Script/js/meu-arquivo.js
@@ -0,0 +1,4 @@
+
+    window.alert('Minha Primeira Mensagem') //Aqui vai uma mensagem de alerta
+    window.confirm('Está gostando de JS?') //Aqui é uma mensagem de confirmação
+    window.prompt('Qual é seu nome?') //Aqui é uma interatividade
