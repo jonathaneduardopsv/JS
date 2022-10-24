318e7ca2f9ab9df589abe1ea638ef0030e801613
Jonathan Eduardo <106741029+jonathaneduardopsv@users.noreply.github.com>
2022-10-24 11:00:20 -0300

    Sistemas de Multa

---
+++ Aulas/JS/Aula 4 - Condições/Exercicio 008.js
@@ -0,0 +1,8 @@
+console.log('O console funcionou corretamente')
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} Km/H`)
+if(vel >60) { 
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+} //Condição Simples!
+console.log(`Dirija sem pre usando cinto de segurança!`)
+
